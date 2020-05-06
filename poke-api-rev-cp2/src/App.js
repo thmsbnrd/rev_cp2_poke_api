e128f8dc6e4d350c8de29f95715ddc69e7757b19
Thomas BENARD <thomas0benard@gmail.com>
2020-05-06 12:07:27 +0200

    imported ListOfPokemon & Pokemon then Routed each pages in App

--- poke-api-rev-cp2/src/App.js
+++ poke-api-rev-cp2/src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Route, Switch } from 'react-router-dom';
+
+import ListOfPokemon from './components/ListOfPokemon';
+import Pokemon from './components/Pokemon';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-
+      <Switch>
+        <Route exact path='/' component={ListOfPokemon} />
+        <Route exact path='/pokemon/:id' component={Pokemon} />
+      </Switch>
     </div>
   );
 }
