9ca5207c7b223faaf338e1199bef7904c4b65872
Thomas BENARD <thomas0benard@gmail.com>
2020-05-06 13:25:58 +0200

    installed axios then called pokemon API in ListOfPokemon

--- poke-api-rev-cp2/src/components/ListOfPokemon.js
+++ poke-api-rev-cp2/src/components/ListOfPokemon.js
@@ -1,6 +1,15 @@
 import React, { Component } from 'react';
+import Axios from 'axios';
 
 class ListOfPokemon extends Component {
+    state = {
+        pokemon: []
+    }
+
+    componentDidMount() {
+        Axios.get('https://pokeapi.co/api/v2/pokemon')
+    }
+
     render() {
         return (
             <div>Page List Pokemon</div>
