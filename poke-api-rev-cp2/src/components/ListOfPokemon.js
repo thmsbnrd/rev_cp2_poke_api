5407e645e567a9a4fbef5ce6b95d220a79f0d7e0
Thomas BENARD <thomas0benard@gmail.com>
2020-05-06 12:38:18 +0200

    started inside ListOfPokemon & Pokemon

---
+++ poke-api-rev-cp2/src/components/ListOfPokemon.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class ListOfPokemon extends Component {
+    render() {
+        return (
+            <div>Page List Pokemon</div>
+        )
+    }
+}
+
+export default ListOfPokemon;
---
+++ poke-api-rev-cp2/src/components/Pokemon.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Pokemon extends Component {
+    render() {
+        return (
+            <div>Page Pokemon</div>
+        )
+    }
+}
+
+export default Pokemon;
